e1f77592927323a30e3764bcb3485f1feb1e142b
1379 <1379@1379.com>
2022-12-03 15:14:06 +0800

    fix: register theme resources static file

--- handler/route.go
+++ handler/route.go
@@ -42,6 +42,7 @@ func (s *Server) RegisterRouters() {
 			staticRouter.StaticFS("/images", gin.Dir(filepath.Join(s.Config.Sonic.AdminResourcesDir, "images"), false))
 			staticRouter.Use(middleware.NewCacheControlMiddleware(middleware.WithMaxAge(time.Hour*24*7)).CacheControl()).
 				StaticFS(consts.SonicUploadDir, gin.Dir(s.Config.Sonic.UploadDir, false))
+			staticRouter.StaticFS("/themes/", gin.Dir(s.Config.Sonic.ThemeDir, false))
 		}
 		{
 			adminApiRouter := router.Group("/api/admin")
