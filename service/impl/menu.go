178fe922256094cc0b25b24a5d1800c3fc57ea56
Meepoljdx <278914323@qq.com>
2023-10-10 16:30:08 +0800

    Fix update menu report update menu failed because of updateResult.RowsAffected=0

--- service/impl/menu.go
+++ service/impl/menu.go
@@ -45,7 +45,8 @@ func (m *menuServiceImpl) UpdateBatch(ctx context.Context, menuParams []*param.M
 			if err != nil {
 				return WrapDBErr(err)
 			}
-			if updateResult.RowsAffected != 1 {
+			// Mysql下如果没有发生变更RowsAffected会为0，只判断!=1会导致报错
+			if updateResult.RowsAffected != 1 && updateResult.RowsAffected != 0 {
 				return xerr.NoType.New("update menu failed").WithMsg("update menu failed").WithStatus(xerr.StatusInternalServerError)
 			}
 		}
